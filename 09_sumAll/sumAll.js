2b29e02baeabc936c42f6e51805fd8ecaed08057
Martu Peluso <martupeluso@proton.me>
2025-07-10 00:49:59 -0300

    Add solution to exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
 
+    let largerNumber;
+    let lowerNumber;
+    
+    if (num1 > num2){
+        largerNumber = num1;
+        lowerNumber = num2;
+    }else{
+        largerNumber = num2;
+        lowerNumber = num1;
+    }
+
+    let sum = 0;
+
+    for (let i = lowerNumber; i <= largerNumber; i++){
+        sum += i;
+    };
+
+    return sum;
 };
 
 // Do not edit below this line
