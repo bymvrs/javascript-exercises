98e5ef4a9dc6a25dc43408b32f20384a31c4e0f7
Martu Peluso <martupeluso@proton.me>
2025-07-21 16:41:02 -0300

    Add solution to exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciMember) {
+    if (fibonacciMember == 0) return 0;
+    if (fibonacciMember < 0) return "OOPS";
 
+    let arr = [1, 1];
+
+    for (let i = 2; i < fibonacciMember; i++){
+        let sum = arr[arr.length - 1] + arr[arr.length - 2]
+        arr.push(sum);
+    };
+
+    return arr[fibonacciMember - 1];
 };
 
 // Do not edit below this line
