b6602658966e5a78b402ac62c50115407f47068a
Martu Peluso <martupeluso@proton.me>
2025-07-21 02:03:58 -0300

    Add solution to exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let alphanumerical = "abcdefghijklmnopqrstuvxyz0123456789";
 
+    let newString = string.toLowerCase().split("").filter(character => alphanumerical.includes(character)).join("");
+    let reversedString = newString.split("").reverse().join("");
+
+    return newString == reversedString;
 };
 
 // Do not edit below this line
