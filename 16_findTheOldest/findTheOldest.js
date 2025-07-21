ca429df23d0a6ca24720efcf920f2cb62a5b329b
Martu Peluso <martupeluso@proton.me>
2025-07-21 17:29:23 -0300

    Add solution to exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const getAge = function (person){
+    const currentYear = new Date().getFullYear();
+    person.yearOfDeath ??= currentYear;
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function (people) {
+  const sortedByAge = people.toSorted((a, b) => getAge(b) - getAge(a));
+  const oldestPerson = sortedByAge[0];
+  return oldestPerson;
 };
 
 // Do not edit below this line
